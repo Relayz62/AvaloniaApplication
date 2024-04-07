c5ac5fe71e9e37a117f81f0ce7c932b21bcd5085
Даниил Горячев <goriachev@pulsarm.ru>
2024-04-07 15:02:25 +0300

    Добавьте файлы проекта.

---
+++ AvaloniaApplication/ViewModels/MainViewModel.cs
@@ -0,0 +1,36 @@
+﻿using ReactiveUI;
+
+namespace AvaloniaApplication.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+
+    public MainViewModel()
+    {
+        OpenPaneCommand = ReactiveCommand.Create(OpenPane);
+        CurrentView = new HomeViewModel();
+    }
+
+    private bool _isPaneOpen = false;
+
+    private ViewModelBase _currentView;
+
+    public ViewModelBase CurrentView
+    {
+        get => _currentView;
+        set => this.RaiseAndSetIfChanged(ref _currentView, value);
+    }
+
+    public bool IsPaneOpen
+    {
+        get => _isPaneOpen;
+        set => this.RaiseAndSetIfChanged(ref _isPaneOpen, value);
+    }
+
+    public IReactiveCommand OpenPaneCommand { get; private set; }
+
+    private void OpenPane()
+    {
+        IsPaneOpen = !IsPaneOpen;
+    }
+}
